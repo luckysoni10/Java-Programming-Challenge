77bb80c1b95145a5732e151a2b93a439a0be2704
Lucky Soni <sonilucky0005@gmail.com>
2025-01-19 09:07:44 +0530

    Added Week 02: Patterns: Inverted Star Pattern

---
+++ Week_02/Patterns/Inverted_Star.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Inverted_Star {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a no. : ");
+        int n = sc.nextInt();
+
+        for(int line = 1; line <=n; line++) {
+            for(int star = 1; star<=(n - line + 1); star++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// Output-
+// Enter a no. : 5
+// *****
+// ****
+// ***
+// **
+// *
