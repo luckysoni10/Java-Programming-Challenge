e29829f6bbc38592efd4f1d414ff7f4a87a0d8e2
Lucky Soni <sonilucky0005@gmail.com>
2025-01-21 11:36:16 +0530

    Added Week 02: Patterns: Inverted Half Pyramid Pattern

---
+++ Week_02/Patterns/Inverted_Half_Pyramid.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Inverted_Half_Pyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of pattern in terms of rows : ");
+        int n = sc.nextInt();
+
+
+        print(n);
+    }
+
+    public static void print(int n) {
+        for(int line = 1; line<= n; line++) {
+            for(int space = 1; space <= n-line; space++) {
+                System.out.print(" ");
+            }
+
+            for(int star = 1; star <= line; star++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+}
+
+// Output -
+// Enter the size of pattern in terms of rows : 5
+//     *
+//    **
+//   ***
+//  ****
+// *****
