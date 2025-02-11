28d2ec631bf5071dbcc8a0815016cc7c6ee1e016
Lucky Soni <sonilucky0005@gmail.com>
2025-02-11 23:12:44 +0530

    Added Week 05

---
+++ Week_05/Bit_Manipulation/Binary_Operators.java
@@ -0,0 +1,9 @@
+public clas Binary_Operator {
+    public static void main(String[] args) {
+        //Binary AND
+        System.out.println("5 & 6 = "+ 5 & 6);
+
+        //Binary OR
+        System.out.println("5 | 6 = "+ 5 | 6);
+    }
+}
