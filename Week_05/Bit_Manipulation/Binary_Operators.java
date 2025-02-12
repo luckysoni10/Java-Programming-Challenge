0eee21d736c539be3491f050718f5675902b8011
Lucky Soni <sonilucky0005@gmail.com>
2025-02-12 08:16:30 +0530

    Added Week 05: Bit Manipulation: Binary Operator

--- Week_05/Bit_Manipulation/Binary_Operators.java
+++ Week_05/Bit_Manipulation/Binary_Operators.java
@@ -1,9 +1,37 @@
-public clas Binary_Operator {
+import java.util.*;
+public class Binary_Operators {
     public static void main(String[] args) {
         //Binary AND
-        System.out.println("5 & 6 = "+ 5 & 6);
+        System.out.print("\nBinary AND 5 & 6 = ");
+        System.out.print(5 & 6);
 
         //Binary OR
-        System.out.println("5 | 6 = "+ 5 | 6);
+        System.out.print("\nBinary OR 5 | 6 = ");
+        System.out.print(5 | 6);
+
+        //Binary XOR
+        System.out.print("\nBinary XOR 5 ^ 6 = ");
+        System.out.print(5 ^ 6);
+
+        //Binary One's Compliment
+        System.out.print("\nBinary 1's Compliment ~5 = ");
+        System.out.print(~5);
+
+        //Binary Left Shift
+        System.out.print("\nBinary Left Shift 5 << 2 = ");
+        System.out.print(5 << 2);
+
+        //Binary Right Shift
+        System.out.print("\nBinary Right Shift 5 >> 2 = ");
+        System.out.print(5 >> 2);
+  
     }
 }
+
+// Output- 
+// Binary AND 5 & 6 = 4
+// Binary OR 5 | 6 = 7
+// Binary XOR 5 ^ 6 = 3
+// Binary 1's Compliment ~5 = -6
+// Binary Left Shift 5 << 2 = 20
+// Binary Right Shift 5 >> 2 = 1
