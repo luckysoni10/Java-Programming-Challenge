d02f4f17a758242e41fb3a77bdcabbd652777c74
Lucky Soni <sonilucky0005@gmail.com>
2025-02-15 07:16:22 +0530

    Added Week 5: Bit Manipulation: Add 1 to an integer

---
+++ Week_05/Bit_Manipulation/AddOne.java
@@ -0,0 +1,15 @@
+//Add 1 to an integer
+public class AddOne {
+    public static void main(String[] args) {
+        int x = 6;
+        System.out.println(x+" + "+1+" is "+-~x);
+        x = -4;
+        System.out.println(x+" + "+1+" is "+-~x);
+        x = 0;
+        System.out.println(x+" + "+1+" is "+-~x);
+    }
+}
+// Output-7
+// 6 + 1 is 7
+// -4 + 1 is -3
+// 0 + 1 is 1
