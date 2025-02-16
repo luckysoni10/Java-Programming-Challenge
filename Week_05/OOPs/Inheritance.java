b4bd6d73cd38d1b811ca9e93ccb405242a29032a
Lucky Soni <sonilucky0005@gmail.com>
2025-02-16 09:49:36 +0530

    Added Week 05: OOPs: Inheritance

---
+++ Week_05/OOPs/Inheritance.java
@@ -0,0 +1,32 @@
+public class Inheritance {
+    public static void main(String[] args) {
+        Fish shark = new Fish();
+        shark.eat();
+        shark.swim();
+    }
+}
+
+//Base Class
+class Animal {
+    String color;
+    void eat () {
+        System.out.println("Eats.");
+    }
+
+    void breathe () {
+        System.out.println("Breathe.");
+    }
+}
+
+//Derived Class
+class Fish extends Animal {
+    int fins;
+
+    void swim () {
+        System.out.println("Swims.");
+    }
+}
+
+// Output-
+// Eats.
+// Swims.
