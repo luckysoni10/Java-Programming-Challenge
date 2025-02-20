be60a3d06040de4ef0655fe860c43b7a43928ac1
Lucky Soni <sonilucky0005@gmail.com>
2025-02-20 23:56:15 +0530

    Added Wek 06: Recursion: Print no. in decreasing order

--- Week_06/Recursion/PrintNo.java
+++ Week_06/Recursion/PrintNo.java
@@ -3,8 +3,11 @@
 public class PrintNo {
     public static void main(String[] args) {
         int n = 10;
+        printInc(n);
+        System.out.println();
         printDec(n);
     }
+
     public static void printDec (int n) {
         if(n==1) {
             System.out.print(n);
@@ -13,7 +16,17 @@ public class PrintNo {
         System.out.print(n+" ");
         printDec(n-1);
     }
+
+    public static void printInc (int n) {
+        if(n==1) {
+            System.out.print(n+" ");
+            return;
+        }
+        printInc(n-1);
+        System.out.print(n+" ");
+    }
 }
 
 //output-
-//10 9 8 7 6 5 4 3 2 1
+// 1 2 3 4 5 6 7 8 9 10 
+// 10 9 8 7 6 5 4 3 2 1
