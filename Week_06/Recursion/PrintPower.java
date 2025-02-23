264c423d1dc662266b76a2975d7217b9f9f8681a
Lucky Soni <sonilucky0005@gmail.com>
2025-02-23 09:03:45 +0530

    Added Week 06: Recursion: Print Power

--- Week_06/Recursion/PrintPower.java
+++ Week_06/Recursion/PrintPower.java
@@ -2,13 +2,28 @@
 public class PrintPower {
     public static void main(String[] args) {
         System.out.println(pow(2,10));
+        System.out.println(OptimizedPow(2,10));
     }
-    public static int pow(int x, int n) {
+    public static int pow(int x, int n) { //O(n)
         if(n == 0) {
             return 1;
         }
         return x * pow(x, n-1);
     }
+    public static int OptimizedPow(int x, int n) { //O(log n)
+        if(n == 0){
+            return 1;
+        }
+        int halfPower = OptimizedPow(x, n/2);
+        int halfPowerSq = halfPower * halfPower;
+
+        //n is odd
+        if(n % 2 != 0 ) {
+            halfPowerSq = x * halfPowerSq;
+        }
+        return halfPowerSq;
+    }
 }
 //Output-
+//1024
 //1024
